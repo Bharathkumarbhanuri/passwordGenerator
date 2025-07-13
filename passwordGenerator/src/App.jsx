b91aa25386d6429edd33e8e4583964f144f3d707
BHANURI BHARATH KUMAR <bhanuribharath@gmail.com>
2025-07-13 12:28:09 +0530

    2nd one

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
   return (
     <>
      <div className="w-full max-w-md mx-auto shadow-md rounded-lg 
-     px-4 py-3 my-8 bg-gray-600 text-orange-500" >
+     px-4 py-3 my-40 bg-gray-600 text-orange-500" >
         <h1 className='text-white text-center my-3'>password Generator</h1>
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
         <input 
